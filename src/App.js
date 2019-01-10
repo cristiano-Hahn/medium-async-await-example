9bf31c88f23a55ca85a6bf42af1172898a49fa11
cristiano-hahn <cristianohahn.ch@gmail.com>
2019-01-10 00:06:24 -0200

    Projeto versionado.

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      inputValue: '89874000',
+      nameOfCity:''
+    }
+  }
+
+
+  
+  async executarPesquisas(){
+    await buscarCidade('89874000')
+    await buscarCidade('89873000')
+  }
+
+  async buscarCidade(numeroDoCep){
+    const url = `https://viacep.com.br/ws/${numeroDoCep}/json/`
+
+    let cidadeEncontrada 
+    
+    await Axios.get(url)
+      .then( (resultado) => cidadeEncontrada = resultado.data.localidade)
+      .catch((erro) => alert("Erro: " + erro))
+
+      alert("Resultado: " + cidadeEncontrada)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type='number'
+          placeholder='Informe um cep'
+          value={this.state.value}
+          onChange={e => this.setState({inputValue: e.target.value})}
+        />  
+
+        <button onClick={() =>this.buscarCidade(this.state.inputValue)}>
+          Buscar nome da cidade
+        </button>
+
+        <h1>{this.state.nameOfCity}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
